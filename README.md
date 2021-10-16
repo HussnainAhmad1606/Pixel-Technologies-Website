5872b4eb666d35fdaf991c7cc1fb7f956c1bb593
Hussnain Ahmad <75199596+HussnainAhmad1606@users.noreply.github.com>
2021-10-16 17:22:26 +0500

    Make Header & README
    Make Navbar, complete hero section, add dark mode functionality and create README file.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pixel Technologies Website
+
+This repository contains the source code for the website of my company Pixel Technologies where we provide Web and Design Solution to the clients.
+
+## Demo
+
+This website is currently in Development Stage. Check out the demo of this website at https://hussnainahmad1606.github.io/Pixel-Technologies-Website/
--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-console.log("This is app.js");
+// DARK MODE 
+let btn = document.getElementById("toggleBtn");
+
+let hamburger = document.getElementById("hamburger")
+btn.addEventListener("change", toggleTheme);
+let body = document.getElementsByTagName("body")[0];
+toggleTheme();
+
+
+function toggleTheme() {
+	if (btn.checked) {
+		body.setAttribute("data-theme", "dark");
+		hamburger.style.color = "white";
+	}
+	else {
+		body.setAttribute("data-theme", "light");
+		hamburger.style.color = "#1d0d77";
+		
+	}
+}
